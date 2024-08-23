243283d1291d368accf5bcb5f69e9c6f2246f033
akash-547 <kashhussain512@gmail.com>
2024-08-24 02:55:35 +0500

    ADD imges and update changes

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["FrontEnd Developer", "Responsive Designer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
